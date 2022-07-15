4af5c158ffd434c3634f6be57ae4a98130c0e157
JoaoKeuwe <joaov2872@hotmail.com>
2022-07-15 02:44:57 -0300

    feat: :art: adicionando controller e service

--- app/backend/src/controller/login.controller.ts
+++ app/backend/src/controller/login.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import UserService from '../services/login.services';
 
 class UserController {
@@ -7,6 +7,17 @@ class UserController {
     const loginService = new UserService();
     const token = await loginService.login(email);
     return res.status(200).json({ token });
+  };
+
+  loginValidate = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { authorization } = req.headers;
+      const service = new UserService();
+      const roles = service.role(authorization);
+      return res.status(200).json(roles);
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
--- app/backend/src/services/login.services.ts
+++ app/backend/src/services/login.services.ts
@@ -1,3 +1,5 @@
+import * as jwt from 'jsonwebtoken';
+import 'dotenv/config';
 import User from '../database/models/user';
 import generateToken from '../middlewares/generateJWT';
 
@@ -14,6 +16,14 @@ class UserService {
       const token = generateToken({ id, username });
       return token;
     }
+  }
+
+  async role(token: string) {
+    const JWT: string = process.env.JWT_SECRET || 'jwt_secret';
+    const generateJWT = jwt.verify(token, JWT) as jwt.JwtPayload;
+    const { password } = generateJWT.payload;
+    const roles = await this.model.findOne(password);
+    return roles;
   }
 }
 export default UserService;
